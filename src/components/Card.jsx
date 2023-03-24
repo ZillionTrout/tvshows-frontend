5bcf5847a88b09f695684c9e6ce20d6a3d02c0fa
Estefania Egea <estefania.egcal@gmail.com>
2023-03-24 19:02:12 +0100

    ite2 done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Card() {
+export default function Card({ show }) {
+
   return (
-    // Use to display each one of the movies
-    <div>Card</div>
+    <div className="cardShow">
+      <img src={show.image} alt={show.title} />
+      <h2>
+        <Link to={`/shows/${show._id}`}>{show.title}</Link>
+      </h2>
+    </div>
   )
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import showService from '../services/showService'
+import Card from '../components/Card';
 import { Link } from 'react-router-dom'
 
 export default function Home() {
+  const [shows, setShows] = useState([]);
+
+  const getShows = async () => {
+  try {
+      const response = await showService.getShows();
+      setShows(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect (() => {
+    getShows();
+  }, [])
 
   return (
     <div>
       <h2>Home</h2>
-      {/* ITERATION 2: Should display a list of all the shows */}
+      {shows.map((elem) => {
+        return (
+          <div key={elem._id}>
+            <Card show={elem} />
+          </div>
+        )
+      })}
     </div>
   )
 }
